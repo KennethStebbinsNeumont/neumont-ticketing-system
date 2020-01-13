772283cfc1f60940046007597e19c2ca4ee71ac2
Kenneth Stebbins <jackk45@gmail.com>
2020-01-12 23:48:42 -0700

    Add project files.

---
+++ Neumont Ticketing System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Neumont_Ticketing_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
