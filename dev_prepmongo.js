4268e88d97293def58f812c52c947b057b427cc6
Kenneth Stebbins <jackk45@gmail.com>
2020-01-24 17:03:07 -0700

    Added mongo prep script

---
+++ dev_prepmongo.js
@@ -0,0 +1,13 @@
+// Create mongo connection
+let mongo = new Mongo();
+
+// Remove old databases
+let db = mongo.getDB("test");
+let oldDbs = db.adminCommand('listDatabases')
+for (let i = 0; i < oldDbs.length; i++) {
+    oldDbs[i].dropDatabase();
+}
+
+// Insert test records
+db = mongo.getDB("test");
+db.tset.insert({ "name": "Hemlo World!", "value": "Doller bills." });
