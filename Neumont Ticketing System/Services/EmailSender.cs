e2b63100e158b606a82ff13183421fcd3e55e55a
Kenneth Stebbins <jackk45@gmail.com>
2020-01-27 13:21:00 -0700

    Temporarily replaced EmailSender.SendEmailAsync() with dummy method

--- Neumont Ticketing System/Services/EmailSender.cs
+++ Neumont Ticketing System/Services/EmailSender.cs
@@ -25,47 +25,53 @@ namespace Neumont_Ticketing_System.Services
             _env = env;
         }
 
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            try
-            {
-                var message = new MimeMessage();
-
-                message.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.Sender));
-
-                message.To.Add(new MailboxAddress(email));
-
-                message.Subject = subject;
-
-                message.Body = new TextPart("html")
-                {
-                    Text = htmlMessage
-                };
-
-                using (var client = new SmtpClient())
-                {
-                    // TODO: Don't accept just any certificate
-                    client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-
-                    if(_env.IsDevelopment())
-                    {
-                        await client.ConnectAsync(_emailSettings.MailServer, _emailSettings.MailPort, true);
-                    } else
-                    {
-                        await client.ConnectAsync(_emailSettings.MailServer);
-                    }
-
-                    await client.AuthenticateAsync(_emailSettings.Sender, _emailSettings.Password);
-
-                    await client.SendAsync(message);
-
-                    await client.DisconnectAsync(true);
-                }
-            } catch (Exception ex)
-            {
-                // TODO: Handle exception
-                throw new InvalidOperationException(ex.Message);
-            }
+            return new Task(() => Console.WriteLine("We should have sent an email, but we're just going to ignore that for now."));
         }
+
+
+        //public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        //{
+        //    try
+        //    {
+        //        var message = new MimeMessage();
+
+        //        message.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.Sender));
+
+        //        message.To.Add(new MailboxAddress(email));
+
+        //        message.Subject = subject;
+
+        //        message.Body = new TextPart("html")
+        //        {
+        //            Text = htmlMessage
+        //        };
+
+        //        using (var client = new SmtpClient())
+        //        {
+        //            // TODO: Don't accept just any certificate
+        //            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+        //            if(_env.IsDevelopment())
+        //            {
+        //                await client.ConnectAsync(_emailSettings.MailServer, _emailSettings.MailPort, true);
+        //            } else
+        //            {
+        //                await client.ConnectAsync(_emailSettings.MailServer);
+        //            }
+
+        //            await client.AuthenticateAsync(_emailSettings.Sender, _emailSettings.Password);
+
+        //            await client.SendAsync(message);
+
+        //            await client.DisconnectAsync(true);
+        //        }
+        //    } catch (Exception ex)
+        //    {
+        //        // TODO: Handle exception
+        //        throw new InvalidOperationException(ex.Message);
+        //    }
+        //}
     }
 }
