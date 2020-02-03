825c8a354862f6d53226340d374219d3c1c2a584
Kenneth Stebbins <jackk45@gmail.com>
2020-02-02 18:58:53 -0700

    Screwing around with views, starting InteractiveList

---
+++ Neumont Ticketing System/Views/Shared/InteractiveList/InteractiveList.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System.Views.Shared
+{
+    public class InteractiveList<T>
+    {
+        public List<T> list { get; set; }
+    }
+}
