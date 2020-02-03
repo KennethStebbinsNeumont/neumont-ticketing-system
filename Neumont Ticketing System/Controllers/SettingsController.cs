ac9858116dc47b416691de805ee821a7d2b9d4f9
Kenneth Stebbins <jackk45@gmail.com>
2020-02-02 20:02:54 -0700

    Attempting to pass data through SettingsController into interactive list in AssetDef.cshtml

--- Neumont Ticketing System/Controllers/SettingsController.cs
+++ Neumont Ticketing System/Controllers/SettingsController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Neumont_Ticketing_System.Services;
+using Neumont_Ticketing_System.Views.Settings;
 
 namespace Neumont_Ticketing_System.Controllers
 {
     public class SettingsController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly AssetsDatabaseService _assetDatabaseService;
+
+        public SettingsController(ILogger<SettingsController> logger,
+            AssetsDatabaseService assetsDatabaseService)
+        {
+            _logger = logger;
+
+            _assetDatabaseService = assetsDatabaseService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +30,11 @@ namespace Neumont_Ticketing_System.Controllers
 
         public IActionResult AssetDef()
         {
-            return View();
+            var assetDefModel = new AssetDefModel(_assetDatabaseService.GetTypes().ToList(),
+                _assetDatabaseService.GetManufacturers().ToList(),
+                _assetDatabaseService.GetModels().ToList());
+
+            return View(assetDefModel);
         }
     }
 }
---
+++ Neumont Ticketing System/Views/Settings/AssetDef.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Neumont_Ticketing_System.Models.Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System.Views.Settings
+{
+    public class AssetDefModel : PageModel
+    {
+        public List<AssetType> AssetTypes { get; private set; }
+        public List<AssetManufacturer> AssetManufacturers { get; private set; }
+        public List<AssetModel> AssetModels { get; private set; }
+
+        public AssetDefModel(List<AssetType> assetTypes,
+            List<AssetManufacturer> assetManufacturers,
+            List<AssetModel> assetModels)
+        {
+            AssetTypes = assetTypes;
+            AssetManufacturers = assetManufacturers;
+            AssetModels = assetModels;
+        }
+
+
+    }
+}
