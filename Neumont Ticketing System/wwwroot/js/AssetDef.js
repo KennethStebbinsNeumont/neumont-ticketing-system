e28e6a50359ff1a81dd3fab8581b4add208a6236
Kenneth Stebbins <jackk45@gmail.com>
2020-02-03 19:46:17 -0700

    Changed phoneKeypress to not check to ensure field has value, since keypress() isn't triggered by keys like backspace or delete

--- Neumont Ticketing System/wwwroot/js/AssetDef.js
+++ Neumont Ticketing System/wwwroot/js/AssetDef.js
@@ -2,12 +2,10 @@
     // This event is triggered when the user exits this field or
     // presses enter AND the value of this element has changed since
     // the last event call
-    console.log("Pretzel");
     if (!$(this).val() &&
         $(this).parent().children().last().attr('id') !== $(this).attr('id')) {
         // If this field is now empty, remove it UNLESS it is the last
         // input box, which is supposed to be blank by default
-        console.log("Doughnut");
         $(this).remove();
     }
 };
@@ -15,12 +13,9 @@
 let phoneKeypress = function () {
     // This event is triggered every time a key is pressed while this field
     // is in focus. This handler is only active for the last input in the list
-    console.log("Banana");
-    if ($(this).parent().children().last().attr('id') === $(this).attr('id') &&
-        $(this).val()) {
+    if ($(this).parent().children().last().attr('id') === $(this).attr('id')) {
         // If this element is the last element in the list and
         // it now has a value, create a new field
-        console.log("Pineapple")
 
         let regexResults = $(this).attr('id').match(/mfr?(\d+)_PhoneNumbers_(\d+)/);
         let mfrIndex = parseInt(regexResults[1]);
