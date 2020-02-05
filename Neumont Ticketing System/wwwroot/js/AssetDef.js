6f9a728b4c0276eff1c10c934e694efdd938e228
Kenneth Stebbins <jackk45@gmail.com>
2020-02-04 22:13:00 -0700

    Attempting one method to add the appropriate handlers to cloned list items

--- Neumont Ticketing System/wwwroot/js/AssetDef.js
+++ Neumont Ticketing System/wwwroot/js/AssetDef.js
@@ -36,24 +36,32 @@ let expandableListOnChange = function () {
     }
 };
 
-let btnAddListItemHandler = function () {
-    let btnContainer = $(this).parent();
-    let list = btnContainer.parent();
-    // Gets the first list item in the button's containing list to use as a template
-    let template = list.children('.listItem').first();
+// Does some cloning work to clone the first item in the list and add it to the
+// end, before the button. Afterwards, it calls the function in afterCloneAction, if it
+// exists, with the new list item as the only parameter
+let getBtnAddListItemHandler = function (afterCloneAction) {
+    return function () {
+        let btnContainer = $(this).parent();
+        let list = btnContainer.parent();
+        // Gets the first list item in the button's containing list to use as a template
+        let template = list.children('.listItem').first();
 
-    // Insert a clone of the template into the list, right before the button's container
-    let clone = $(template.clone());
-    // Clear the values of all input fields
-    clone.find('input').each(function (i, e) {
-        $(e).val('');
-    });
-    // https://stackoverflow.com/questions/1414276/how-to-make-the-first-option-of-select-selected-with-jquery
-    clone.find('select').each(function (i, e) {
-        let firstOption = $(e).find('option').first();
-        $(e).val(firstOption.val());
-    });
-    $(clone).insertBefore(btnContainer);
+        // Insert a clone of the template into the list, right before the button's container
+        let clone = $(template.clone());
+        // Clear the values of all input fields
+        clone.find('input').each(function (i, e) {
+            $(e).val('');
+        });
+        // https://stackoverflow.com/questions/1414276/how-to-make-the-first-option-of-select-selected-with-jquery
+        clone.find('select').each(function (i, e) {
+            let firstOption = $(e).find('option').first();
+            $(e).val(firstOption.val());
+        });
+        $(clone).insertBefore(btnContainer);
+
+        if(afterCloneAction)
+            afterCloneAction(clone);
+    }
 }
 
 $(document).ready(() => {
@@ -103,9 +111,17 @@ $(document).ready(() => {
 
     const mfrNameInputs = $('#manufacturersList').find('.nameInput');
 
-    const addListItemButtons = $('.btnAddListItem');
+    typesList.find('.btnAddListItem').click(getBtnAddListItemHandler(function (clone) {
+        let c = $(clone);
+        c.change(expandableListOnChange);
+        c.change(onTypeNameChange);
+        c.blur(expandableListOnChange);
+        c.blur(onTypeNameChange);
+        c.keypress(expandableListOnKeypress);
+    }));
+    //const addListItemButtons = $('.btnAddListItem');
 
-    addListItemButtons.each(function (index, btn) {
-        $(btn).click(btnAddListItemHandler);
-    });
+    //addListItemButtons.each(function (index, btn) {
+    //    $(btn).click(btnAddListItemHandler);
+    //});
 });
