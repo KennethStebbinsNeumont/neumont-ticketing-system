5440cd5f766d3823d5d496fda35b975253167068
Kenneth Stebbins <jackk45@gmail.com>
2020-02-07 11:40:40 -0700

    First draft of asset manager front end

---
+++ Neumont Ticketing System/Views/Settings/AssetManager.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Neumont_Ticketing_System.Models.Assets;
+using Neumont_Ticketing_System.Models.Owners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System.Views.Settings
+{
+    public class AssetManagerModel : PageModel
+    {
+        public List<AssetType> AssetTypes { get; private set; }
+        public List<AssetManufacturer> AssetManufacturers { get; private set; }
+        public List<AssetModel> AssetModels { get; private set; }
+        public List<Asset> Assets { get; private set; }
+        public List<Owner> Owners { get; private set; }
+
+        public AssetManagerModel(List<AssetType> assetTypes,
+            List<AssetManufacturer> assetManufacturers,
+            List<AssetModel> assetModels,
+            List<Asset> assets,
+            List<Owner> owners)
+        {
+            AssetTypes = assetTypes;
+            AssetManufacturers = assetManufacturers;
+            AssetModels = assetModels;
+            Assets = assets;
+            Owners = owners;
+        }
+
+
+    }
+}
