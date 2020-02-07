864f029ec9d68fad648c5ae56195bcfea8707a16
Kenneth Stebbins <jackk45@gmail.com>
2020-02-07 11:25:26 -0700

    Implemented GetOwnedAssets to allow for easier collection of owned assets

--- Neumont Ticketing System/Models/Owners/Owner.cs
+++ Neumont Ticketing System/Models/Owners/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Neumont_Ticketing_System.Models.Assets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,5 +23,21 @@ namespace Neumont_Ticketing_System.Models.Owners
         public List<string> PhoneNumbers { get; set; }
 
         public PreferredName PreferredName { get; set; }
+
+        public List<Asset> GetOwnedAssets(List<Asset> allAssets)
+        {
+            return GetOwnedAssets(this, allAssets);
+        }
+
+        public static List<Asset> GetOwnedAssets(Owner owner, List<Asset> allAssets)
+        {
+            List<Asset> result = new List<Asset>();
+            foreach(var asset in allAssets)
+            {
+                if(owner.Id.Equals(asset.OwnerId))
+                    result.Add(asset);
+            }
+            return result;
+        }
     }
 }
