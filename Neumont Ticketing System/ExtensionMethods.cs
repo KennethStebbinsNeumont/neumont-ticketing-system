43952173712120e60545870edc0e59db4bf8f60f
Kenneth Stebbins <jackk45@gmail.com>
2020-02-08 21:00:32 -0700

    Added a String extension method to remove special characters

---
+++ Neumont Ticketing System/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System
+{
+    public static class ExtensionMethods
+    {
+        // https://stackoverflow.com/questions/1120198/most-efficient-way-to-remove-special-characters-from-string
+        public static string RemoveSpecialCharacters(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
