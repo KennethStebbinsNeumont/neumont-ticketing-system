7683570adbb084c407fef954df46b9b73431b442
Kenneth Stebbins <jackk45@gmail.com>
2020-02-08 21:57:48 -0700

    Added some error handling

--- Neumont Ticketing System/Controllers/SettingsController.cs
+++ Neumont Ticketing System/Controllers/SettingsController.cs
@@ -183,78 +183,106 @@ namespace Neumont_Ticketing_System.Controllers
         [HttpPost]
         public async Task<JsonResult> AssetManager(AssetManagerQuery query)
         {
-            var result = Task.Run(() =>
+            var result = Task.Run<JsonResult>(() =>
             {
-                string[] words = query.Query.Split(' ');
-                string possibleSerialNumber = query.Query.RemoveSpecialCharacters();
-                List<Asset> matchedAssets = _assetDatabaseService.GetAssets(a => a.SerialNumber
-                    .Contains(possibleSerialNumber));
-
-                List<Owner> matchedOwners = new List<Owner>();
-                foreach (string word in words)
+                if(query.Query == null || query.Query == "")
                 {
-                    matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.Name.Contains(word)));
-                    matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.PreferredName.First.Contains(word)));
-                    matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.PreferredName.Middle.Contains(word)));
-                    matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.PreferredName.Last.Contains(word)));
-                }
-
-                List<Asset> tempMatchedAssets = new List<Asset>();
-                foreach (Owner owner in matchedOwners)
-                {
-                    tempMatchedAssets = _assetDatabaseService.GetAssets(a => a.OwnerId.Equals(owner.Id));
-                    foreach (Asset asset in tempMatchedAssets)
+                    _logger.LogError("The given query was null or empty.");
+                    return new JsonResult(new AssetManagerQueryResponse
                     {
-                        if (!matchedAssets.Contains(asset))
-                            matchedAssets.Add(asset);
-                    }
-                }
-
-                List<AssetManagerQueryResponseAsset> responseAssets = new List<AssetManagerQueryResponseAsset>();
-                Owner matchedOwner = null;
-                AssetModel matchedModel = null;
-                AssetType matchedType = null;
-                foreach (var owner in matchedOwners)
+                        Successful = false,
+                        Message = "The query string was null or empty.",
+                        Query = query.Query,
+                        Assets = new List<AssetManagerQueryResponseAsset>()
+                    });
+                } else
                 {
-                    foreach (var asset in _assetDatabaseService.GetAssets(a => a.OwnerId.Equals(owner.Id)))
+                    try
                     {
-                        matchedModel = _assetDatabaseService.GetModelById(asset.ModelId);
-                        matchedType = _assetDatabaseService.GetTypeById(matchedModel.TypeId);
-                        responseAssets.Add(new AssetManagerQueryResponseAsset
+                        string[] words = query.Query.Split(' ');
+                        string possibleSerialNumber = query.Query.RemoveSpecialCharacters();
+                        List<Asset> matchedAssets = _assetDatabaseService.GetAssets(a => a.SerialNumber
+                            .Contains(possibleSerialNumber));
+
+                        List<Owner> matchedOwners = new List<Owner>();
+                        foreach (string word in words)
                         {
-                            OwnerId = asset.OwnerId,
-                            OwnerName = owner.Name,
-                            OwnerPreferredName = owner.PreferredName,
-                            AssetId = asset.Id,
-                            AssetSerial = asset.SerialNumber,
-                            AssetModelName = matchedModel.Name,
-                            AssetTypeName = matchedType.Name
+                            matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.Name.Contains(word)));
+                            matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.PreferredName.First.Contains(word)));
+                            matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.PreferredName.Middle.Contains(word)));
+                            matchedOwners.AddRange(_ownersDatabaseService.GetOwners(o => o.PreferredName.Last.Contains(word)));
+                        }
+
+                        List<Asset> tempMatchedAssets = new List<Asset>();
+                        foreach (Owner owner in matchedOwners)
+                        {
+                            tempMatchedAssets = _assetDatabaseService.GetAssets(a => a.OwnerId.Equals(owner.Id));
+                            foreach (Asset asset in tempMatchedAssets)
+                            {
+                                if (!matchedAssets.Contains(asset))
+                                    matchedAssets.Add(asset);
+                            }
+                        }
+
+                        List<AssetManagerQueryResponseAsset> responseAssets = new List<AssetManagerQueryResponseAsset>();
+                        Owner matchedOwner = null;
+                        AssetModel matchedModel = null;
+                        AssetType matchedType = null;
+                        foreach (var owner in matchedOwners)
+                        {
+                            foreach (var asset in _assetDatabaseService.GetAssets(a => a.OwnerId.Equals(owner.Id)))
+                            {
+                                matchedModel = _assetDatabaseService.GetModelById(asset.ModelId);
+                                matchedType = _assetDatabaseService.GetTypeById(matchedModel.TypeId);
+                                responseAssets.Add(new AssetManagerQueryResponseAsset
+                                {
+                                    OwnerId = asset.OwnerId,
+                                    OwnerName = owner.Name,
+                                    OwnerPreferredName = owner.PreferredName,
+                                    AssetId = asset.Id,
+                                    AssetSerial = asset.SerialNumber,
+                                    AssetModelName = matchedModel.Name,
+                                    AssetTypeName = matchedType.Name
+                                });
+                            }
+                        }
+
+                        foreach (var asset in matchedAssets)
+                        {
+                            matchedOwner = _ownersDatabaseService.GetOwnerById(asset.OwnerId);
+                            matchedModel = _assetDatabaseService.GetModelById(asset.ModelId);
+                            matchedType = _assetDatabaseService.GetTypeById(matchedModel.TypeId);
+                            responseAssets.Add(new AssetManagerQueryResponseAsset
+                            {
+                                OwnerId = asset.OwnerId,
+                                OwnerName = matchedOwner.Name,
+                                OwnerPreferredName = matchedOwner.PreferredName,
+                                AssetId = asset.Id,
+                                AssetSerial = asset.SerialNumber,
+                                AssetModelName = matchedModel.Name,
+                                AssetTypeName = matchedType.Name
+                            });
+                        }
+
+                        return new JsonResult(new AssetManagerQueryResponse
+                        {
+                            Successful = true,
+                            Message = "Query completed normally.",
+                            Query = query.Query,
+                            Assets = responseAssets
+                        });
+                    } catch(Exception e)
+                    {
+                        _logger.LogError(e, "Unexpected exception while trying to query database for assets and owners.");
+                        return new JsonResult(new AssetManagerQueryResponse
+                        {
+                            Successful = false,
+                            Message = "An unexpected internal error ocurred.",
+                            Query = query.Query,
+                            Assets = new List<AssetManagerQueryResponseAsset>()
                         });
                     }
                 }
-
-                foreach (var asset in matchedAssets)
-                {
-                    matchedOwner = _ownersDatabaseService.GetOwnerById(asset.OwnerId);
-                    matchedModel = _assetDatabaseService.GetModelById(asset.ModelId);
-                    matchedType = _assetDatabaseService.GetTypeById(matchedModel.TypeId);
-                    responseAssets.Add(new AssetManagerQueryResponseAsset
-                    {
-                        OwnerId = asset.OwnerId,
-                        OwnerName = matchedOwner.Name,
-                        OwnerPreferredName = matchedOwner.PreferredName,
-                        AssetId = asset.Id,
-                        AssetSerial = asset.SerialNumber,
-                        AssetModelName = matchedModel.Name,
-                        AssetTypeName = matchedType.Name
-                    });
-                }
-
-                return new JsonResult(new AssetManagerQueryResponse
-                {
-                    Query = query.Query,
-                    Assets = responseAssets
-                });
             });
 
             return await result;
@@ -475,6 +503,8 @@ namespace Neumont_Ticketing_System.Controllers
 
     public class AssetManagerQueryResponse
     {
+        public bool Successful { get; set; }
+        public string Message { get; set; }
         public string Query { get; set; }
         public List<AssetManagerQueryResponseAsset> Assets { get; set; }
     }
