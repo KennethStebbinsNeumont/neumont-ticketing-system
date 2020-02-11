650216dadd3210e1011f0a076cd140ad17981de2
Kenneth Stebbins <jackk45@gmail.com>
2020-02-10 22:07:09 -0700

    Now supports entire lists of duplicates

--- Neumont Ticketing System/Controllers/Exceptions/DuplicateException.cs
+++ Neumont Ticketing System/Controllers/Exceptions/DuplicateException.cs
@@ -30,28 +30,73 @@ namespace Neumont_Ticketing_System.Controllers.Exceptions
     public class DuplicateException<T> : DuplicateException
     {
         public T Duplicate { get; private set; }
+        public List<T> Duplicates { get; private set; }
+        public int DuplicateCount { get
+            {
+                return Duplicates.Count;
+            } 
+        }
 
         public DuplicateException(T duplicate) : base()
         {
-            Duplicate = duplicate;
+            InitSingle(duplicate);
+        }
+
+        public DuplicateException(List<T> duplicates): base()
+        {
+            InitList(duplicates);
         }
 
         public DuplicateException(T duplicate, string? message) : base(message)
         {
-            Duplicate = duplicate;
+            InitSingle(duplicate);
+        }
+
+        public DuplicateException(List<T> duplicates, string? message) : base(message)
+        {
+            InitList(duplicates);
         }
 
         public DuplicateException(T duplicate, string? message, Exception? innerException)
             : base(message, innerException)
         {
-            Duplicate = duplicate;
+            InitSingle(duplicate);
+        }
+
+        public DuplicateException(List<T> duplicates, string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+            InitList(duplicates);
         }
 
         public DuplicateException(T duplicate, System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context)
             : base(info, context)
         {
+            InitSingle(duplicate);
+        }
+
+        public DuplicateException(List<T> duplicates, System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+            InitList(duplicates);
+        }
+
+        private void InitSingle(T duplicate)
+        {
             Duplicate = duplicate;
+            Duplicates = new List<T>(1);
+            Duplicates.Add(duplicate);
+        }
+
+        private void InitList(List<T> duplicates)
+        {
+            if (duplicates.Count == 0)
+                throw new ArgumentException("Given duplicates list cannot be empty");
+
+            Duplicate = duplicates.First();
+            Duplicates = duplicates;
         }
     }
 }
