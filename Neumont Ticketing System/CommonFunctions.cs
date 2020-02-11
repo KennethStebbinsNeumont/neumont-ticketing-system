98df0c20ad56273547e9af856d8595958d79b375
Kenneth Stebbins <jackk45@gmail.com>
2020-02-10 22:09:06 -0700

    Forgot to commit the addition of the CommonFunctions class with NormalizeString

---
+++ Neumont Ticketing System/CommonFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System
+{
+    public static class CommonFunctions
+    {
+        public static string NormalizeString(string input)
+        {
+            return input.RemoveSpecialCharacters().ToUpper();
+        }
+    }
+}
