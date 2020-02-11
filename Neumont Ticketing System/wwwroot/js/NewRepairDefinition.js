162007c7a337c7ea55f0c57560a92c7026507925
Kenneth Stebbins <jackk45@gmail.com>
2020-02-10 21:07:44 -0700

    I think I called .children() one too many times in the first level of substeps

--- Neumont Ticketing System/wwwroot/js/NewRepairDefinition.js
+++ Neumont Ticketing System/wwwroot/js/NewRepairDefinition.js
@@ -143,7 +143,7 @@ let jsonifyInputs = function jsonifyInputs() {
         for (let i = 0; i < stepContainers.length; i++) {
             steps.push({
                 Name: $(stepContainers[i]).children('.inputContainer').children('.stepName').val(),
-                SubSteps: getSteps($(stepContainers[i]).children('.subStepList').children())
+                SubSteps: getSteps($(stepContainers[i]).children('.subStepList')))
             });
         }
 
