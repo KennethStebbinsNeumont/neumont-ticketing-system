7286a210a792e493b1f1bff8c37b8e310892d7ec
Kenneth Stebbins <jackk45@gmail.com>
2020-02-12 16:25:27 -0700

    Added normalized versions of first, middle, and last names to owners database schema

--- Neumont Ticketing System/Models/PreferredName.cs
+++ Neumont Ticketing System/Models/PreferredName.cs
@@ -8,7 +8,10 @@ namespace Neumont_Ticketing_System.Models
     public class PreferredName
     {
         public string First { get; set; }
+        public string NormalizedFirst { get; set; }
         public string Middle { get; set; }
+        public string NormalizedMiddle { get; set; }
         public string Last { get; set; }
+        public string NormalizedLast { get; set; }
     }
 }
