d3fc795558c3b8c5247b138e9e7bd498ff531370
Kenneth Stebbins <jackk45@gmail.com>
2020-02-16 11:01:37 -0700

    Finished first draft of front end, still needs work though

--- Neumont Ticketing System/Controllers/TicketsController.cs
+++ Neumont Ticketing System/Controllers/TicketsController.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Neumont_Ticketing_System.Areas.Identity.Data;
 using Neumont_Ticketing_System.Models.Assets;
+using Neumont_Ticketing_System.Views.Tickets;
 
 namespace Neumont_Ticketing_System.Controllers
 {
     public class TicketsController : Controller
     {
+        private readonly ILogger<SettingsController> _logger;
+
+        private List<AppUser> _technicians;
+
+        public TicketsController(ILogger<SettingsController> logger,
+            List<AppUser> technicians)
+        {
+            _technicians = technicians;
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,7 +30,9 @@ namespace Neumont_Ticketing_System.Controllers
 
         public IActionResult NewTicket()
         {
-            return View();
+            var model = new NewTicketModel(_technicians);
+
+            return View(model);
         }
 
         [HttpPost]
---
+++ Neumont Ticketing System/wwwroot/js/NewTicket.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+
+    const expandableListInputs = $('.expandableListInput');
+    expandableListInputs.each(function (index, input) {
+        $(input).change(ExpandableInputList.onInputChange);
+        $(input).blur(ExpandableInputList.onInputBlur);
+        $(input).keypress(ExpandableInputList.onInputKeypress);
+    });
+});
