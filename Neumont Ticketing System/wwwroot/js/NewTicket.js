e18ded7188c1a97ffcb954524166a14f978897b6
Kenneth Stebbins <jackk45@gmail.com>
2020-02-16 12:07:34 -0700

    Connected Add Comment button to expandable item list

--- Neumont Ticketing System/wwwroot/js/NewTicket.js
+++ Neumont Ticketing System/wwwroot/js/NewTicket.js
@@ -1,10 +1,13 @@
 ﻿$(document).ready(function () {
-
-
     const expandableListInputs = $('.expandableListInput');
     expandableListInputs.each(function (index, input) {
         $(input).change(ExpandableInputList.onInputChange);
         $(input).blur(ExpandableInputList.onInputBlur);
         $(input).keypress(ExpandableInputList.onInputKeypress);
     });
+
+    $('.btnAddListItem').click(ExpandableItemList.getBtnAddListItemHandler(
+        (clone) => {
+            clone.find('textarea').val('');
+        }));
 });
