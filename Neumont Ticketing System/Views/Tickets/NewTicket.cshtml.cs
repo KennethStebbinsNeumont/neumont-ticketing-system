d177bcd24ac48b634e4bff6dab323a8d063a6c06
Kenneth Stebbins <jackk45@gmail.com>
2020-02-15 18:42:04 -0700

    First draft of new ticket cshtml

--- Neumont Ticketing System/Views/Tickets/NewTicket.cshtml.cs
+++ Neumont Ticketing System/Views/Tickets/NewTicket.cshtml.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Neumont_Ticketing_System.Areas.Identity.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Neumont_Ticketing_System.Views.Tickets
 {
-    public class NewTicket
+    public class NewTicketModel
     {
+        public List<AppUser> Technicians { get; set; }
+
+        public NewTicketModel(List<AppUser> technicians)
+        {
+            Technicians = technicians;
+
+        }
     }
 }
