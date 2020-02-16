d9afaf741327170c636a6d71d29c786842217b04
Kenneth Stebbins <jackk45@gmail.com>
2020-02-15 18:17:41 -0700

    Preparing for back-end of ticket creation page

--- Neumont Ticketing System/Controllers/TicketsController.cs
+++ Neumont Ticketing System/Controllers/TicketsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Neumont_Ticketing_System.Models.Assets;
 
 namespace Neumont_Ticketing_System.Controllers
 {
@@ -16,6 +17,13 @@ namespace Neumont_Ticketing_System.Controllers
         public IActionResult NewTicket()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetApplicableRepairs([FromBody] Asset asset)
+        {
+            //TODO: Implement
+            throw new NotImplementedException();
         }
     }
 }
