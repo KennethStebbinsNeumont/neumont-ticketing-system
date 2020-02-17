c7d86556545f22f598b72eb7b25b2d15e252fe08
Kenneth Stebbins <jackk45@gmail.com>
2020-02-17 12:09:55 -0700

    Added generic NotFoundException

---
+++ Neumont Ticketing System/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System.Services.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public NotFoundException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+
+    public class NotFoundException<T> : NotFoundException
+    {
+        public Type Type => typeof(T);
+        public string TypeName => typeof(T).Name;
+
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundException(string? message, Exception? innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public NotFoundException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
