cbec625f5a5e6bc0c5531c145dadda654f862bd7
Kenneth Stebbins <jackk45@gmail.com>
2020-02-17 19:58:25 -0700

    Implemented new Repair model

---
+++ Neumont Ticketing System/Models/Tickets/Repair.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System.Models.Tickets
+{
+    public class Repair
+    {
+        [BsonElement("Definition")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string DefinitionId { get; set; }
+
+        public List<RepairStep> Steps { get; set; }
+    }
+}
