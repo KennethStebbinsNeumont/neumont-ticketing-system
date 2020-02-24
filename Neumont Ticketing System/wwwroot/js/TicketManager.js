05883c7103b37b4513171ced8ceb39df8c51dbba
Kenneth Stebbins <jackk45@gmail.com>
2020-02-23 17:56:20 -0700

    Starting ticket manager front-end JS

---
+++ Neumont Ticketing System/wwwroot/js/TicketManager.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(() => {
+    // Toggle checkbox when the ticket entry is clicked
+    $('.tableRow').click(function () {
+        let chkbx = $(this).find('.checkbox');
+        chkbx.prop('checked', !chkbx.prop('checked'));
+    });
+
+    // Preserve normal checkbox function
+    $('.checkbox').click(function (event) {
+        event.stopPropagation();
+        this.checked = !this.checked;
+    });
+});
