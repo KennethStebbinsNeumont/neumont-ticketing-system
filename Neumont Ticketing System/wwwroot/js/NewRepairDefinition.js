5b7125b6b23c29f37dd72161a2a1930a86b2dcb2
Kenneth Stebbins <jackk45@gmail.com>
2020-03-01 19:11:50 -0700

    _all selection no longer attempts to preserve previous choices

--- Neumont Ticketing System/wwwroot/js/NewRepairDefinition.js
+++ Neumont Ticketing System/wwwroot/js/NewRepairDefinition.js
@@ -17,16 +17,6 @@
     }
     else if (newVal === '_all') {
         // If the user chose for this to apply to all types/mfrs/models
-        let oldValues = [];
-        // Get all of the current selections
-        parent.find('.expandableListSelector').each(function (i, e) {
-            let ele = $(e);
-            let val = ele.val();
-            if (val && val !== '_all' && !oldValues.includes(val)) {
-                oldValues.push(val);
-            }
-        });
-
         // Put the _all selection at the top, preserve the other selections
         // (but set the elements to disabled), remove excess selectors
         parent.find('.expandableListSelector').each(function (i, e) {
@@ -35,9 +25,6 @@
                 ele.val('_all');
                 ele.attr('disabled', false);
                 ele.attr('old-value', '_all');
-            } else if (i < oldValues.length + 1) {
-                ele.val(oldValues[i + 1]);
-                ele.attr('disabled', true);
             } else {
                 ele.remove();
             }
