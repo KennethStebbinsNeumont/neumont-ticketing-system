530d859b7b056432a22eb673aeec3d624fd2e2d3
Kenneth Stebbins <jackk45@gmail.com>
2020-03-06 16:47:59 -0700

    Now redirects user to settings index page upon submission of new owner

--- Neumont Ticketing System/wwwroot/js/AssetCreator.js
+++ Neumont Ticketing System/wwwroot/js/AssetCreator.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(() => {
+﻿(function () {
     const jsonfiyInputs = function jsonifyInputs() {
         let result = {
             owners: []
@@ -57,75 +57,85 @@
         return result;
     };
 
-    $('.expandableListInput').each(function (i, e) {
-        let ele = $(e);
-        ele.change(ExpandableInputList.onInputChange);
-        ele.blur(ExpandableInputList.onInputBlur);
-        ele.keypress(ExpandableInputList.onInputKeypress);
-    });
+    const submitOwner = async function submitOwner() {
+        try {
+            let response = await $.ajax({
+                type: "POST",
+                url: "/Settings/AssetCreator",
+                data: JSON.stringify(jsonfiyInputs()),
+                contentType: "application/json",
+                dataType: "json"
+            });
 
-    $('.btnAddAsset').each(function (i, e) {
-        let ele = $(e);
-        ele.click(ExpandableItemList.getBtnAddListItemHandler(function (c) {
-            // Choose the first model in the list
-            c.find('.modelSelector').val(c.find('option').first().val());
-        }));
-    });
-
-    let ownerList = $('#ownerList');
-    ownerList.find('.btnNewOwner').click(ExpandableItemList.getBtnAddListItemHandler(
-        function (clone) {
-            let phoneInput = clone.find('.phoneNumberInput');
-            // Remove excessive input fields
-            if (phoneInput.length > 1) {
-                phoneInput.each(function (i, e) {
-                    if (i > 0)
-                        $(e).remove();
-                });
-                phoneInput = clone.find('.phoneNumberInput');
+            if (response.successful) {
+                // Redirect to settings index page on submit
+                window.location.href = "/Settings";
+            } else {
+                console.error(`Unexpected internal server error: ${response.message}`);
             }
-            phoneInput.change(ExpandableInputList.onInputChange);
-            phoneInput.blur(ExpandableInputList.onInputBlur);
-            phoneInput.keypress(ExpandableInputList.onInputKeypress);
+        } catch (e) {
+            console.error("Unexpected error while submitting definitions.");
+            console.error(e);
+        }
+    }
 
-            let emailInput = clone.find('.emailAddressInput');
-            // Remove excessive input fields
-            if (emailInput.length > 1) {
-                emailInput.each(function (i, e) {
-                    if (i > 0)
-                        $(e).remove();
-                });
-                emailInput = clone.find('.emailAddressInput');
-            }
-            emailInput.change(ExpandableInputList.onInputChange);
-            emailInput.blur(ExpandableInputList.onInputBlur);
-            emailInput.keypress(ExpandableInputList.onInputKeypress);
+    $(document).ready(() => {
 
-            clone.find('.btnAddAsset').click(ExpandableItemList.getBtnAddListItemHandler(function (c) {
+        $('.expandableListInput').each(function (i, e) {
+            let ele = $(e);
+            ele.change(ExpandableInputList.onInputChange);
+            ele.blur(ExpandableInputList.onInputBlur);
+            ele.keypress(ExpandableInputList.onInputKeypress);
+        });
+
+        $('.btnAddAsset').each(function (i, e) {
+            let ele = $(e);
+            ele.click(ExpandableItemList.getBtnAddListItemHandler(function (c) {
                 // Choose the first model in the list
                 c.find('.modelSelector').val(c.find('option').first().val());
             }));
-    }, function (clone) {
-            clone.insertAfter(ownerList.find('.btnNewOwnerContainer'));
-    }));
+        });
 
-    // https://www.c-sharpcorner.com/blogs/post-the-data-to-asp-net-mvc-controller-using-jquery-ajax
-    $('#submit').click(function () {
-        $.ajax({
-            type: "POST",
-            url: "/Settings/AssetCreator",
-            data: JSON.stringify(jsonfiyInputs()),
-            contentType: "application/json",
-            dataType: "json",
-            success: function (response) {
-                console.log("Success!");
-            },
-            failure: function (response) {
-                console.log("Failure :(");
-            },
-            error: function (response) {
-                console.log("ERROR!!!!");
-            }
-        })
+        let ownerList = $('#ownerList');
+        ownerList.find('.btnNewOwner').click(
+            ExpandableItemList.getBtnAddListItemHandler(
+                function (clone) {
+                    let phoneInput = clone.find('.phoneNumberInput');
+                    // Remove excessive input fields
+                    if (phoneInput.length > 1) {
+                        phoneInput.each(function (i, e) {
+                            if (i > 0)
+                                $(e).remove();
+                        });
+                        phoneInput = clone.find('.phoneNumberInput');
+                    }
+                    phoneInput.change(ExpandableInputList.onInputChange);
+                    phoneInput.blur(ExpandableInputList.onInputBlur);
+                    phoneInput.keypress(ExpandableInputList.onInputKeypress);
+
+                    let emailInput = clone.find('.emailAddressInput');
+                    // Remove excessive input fields
+                    if (emailInput.length > 1) {
+                        emailInput.each(function (i, e) {
+                            if (i > 0)
+                                $(e).remove();
+                        });
+                        emailInput = clone.find('.emailAddressInput');
+                    }
+                    emailInput.change(ExpandableInputList.onInputChange);
+                    emailInput.blur(ExpandableInputList.onInputBlur);
+                    emailInput.keypress(ExpandableInputList.onInputKeypress);
+
+                    clone.find('.btnAddAsset').click(ExpandableItemList.getBtnAddListItemHandler(function (c) {
+                        // Choose the first model in the list
+                        c.find('.modelSelector').val(c.find('option').first().val());
+                    }));
+                }, function (clone) {
+                    clone.insertAfter(ownerList.find('.btnNewOwnerContainer'));
+            })
+        );
+
+        // https://www.c-sharpcorner.com/blogs/post-the-data-to-asp-net-mvc-controller-using-jquery-ajax
+        $('#submit').click(submitOwner);
     });
-});
+})();
