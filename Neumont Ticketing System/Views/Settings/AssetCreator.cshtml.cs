291214755aab8a4f6fa992342f1d8044a1e42906
Kenneth Stebbins <contact@kennethstebbins.com>
2020-03-23 23:05:59 -0600

    Removed unnecessary imports

--- Neumont Ticketing System/Views/Settings/AssetCreator.cshtml.cs
+++ Neumont Ticketing System/Views/Settings/AssetCreator.cshtml.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using Neumont_Ticketing_System.Models.Assets;
+﻿using Neumont_Ticketing_System.Models.Assets;
 using Neumont_Ticketing_System.Models.Owners;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Neumont_Ticketing_System.Views.Settings
 {
-    public class AssetCreatorModel : PageModel
+    public class AssetCreatorModel
     {
         public List<AssetModel> AssetModels { get; private set; }
         public List<Asset> Assets { get; private set; }
