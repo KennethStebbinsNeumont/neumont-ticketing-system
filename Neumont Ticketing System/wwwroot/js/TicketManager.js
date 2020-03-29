dad88cc1af8ba8cf6587b47541eeb91abda8ceb0
Kenneth Stebbins <contact@kennethstebbins.com>
2020-03-29 13:53:56 -0600

    Styling tweaks
    Added more specific class names to ticket checkboxes and their containers

--- Neumont Ticketing System/wwwroot/js/TicketManager.js
+++ Neumont Ticketing System/wwwroot/js/TicketManager.js
@@ -1,13 +1,13 @@
 ﻿$(document).ready(() => {
     // Toggle checkbox when the ticket entry is clicked
     $('.tableRow').click(function () {
-        let chkbx = $(this).find('.checkbox');
+        let chkbx = $(this).find('.checkTicketSelected');
         chkbx.prop('checked', !chkbx.prop('checked'));
     });
 
     // Prevent checkbox from toggling twice when it is
     // clicked directly
-    $('.checkbox').click(function (event) {
+    $('.checkTicketSelected').click(function (event) {
         event.stopPropagation();
     });
 
