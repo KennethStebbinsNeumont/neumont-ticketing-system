f83afefe08902f6c5cc4533525fecbff3848079e
Kenneth Stebbins <contact@kennethstebbins.com>
2020-03-31 19:28:40 -0600

    Started loaner editor page

---
+++ Neumont Ticketing System/Views/Settings/LoanerEditor.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Neumont_Ticketing_System.Models.Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neumont_Ticketing_System.Views.Settings
+{
+    public class LoanerEditorModel
+    {
+        public LoanerAsset Loaner { get; set; }
+
+        public bool HasLoaner
+        {
+            get
+            {
+                return Loaner != null;
+            }
+        }
+
+        public Asset AssociatedAsset { get; set; }
+
+        public List<AssetModel> AssetModels { get; set; }
+    }
+}
